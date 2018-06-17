41bad99ecba94fd146ed6d5af73d905004d37a86
fortinmeihdi <fortinmeihdi@gmail.com>
2018-06-17 16:38:00 +0200

    inscription/deconnexion
    Ajout du front pour les pages inscription et déconnexion

---
+++ Connexion.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Noron
+ * Date: 17/06/2018
+ * Time: 16:15
+ */
+session_start();
+$titre="Connexion";
+include("includes/identifiants.php");
+include("includes/debut.php");
+include("includes/menu.php");
+?>
