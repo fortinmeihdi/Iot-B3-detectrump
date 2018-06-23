cd31b3d30d4a2c3000e9562f850c0e4f65b2d68f
fortinmeihdi <fortinmeihdi@gmail.com>
2018-06-23 20:40:06 +0200

    Ajout code php

--- Connexion.php
+++ Connexion.php
@@ -2,12 +2,24 @@
 /**
  * Created by PhpStorm.
  * User: Noron
- * Date: 17/06/2018
- * Time: 16:15
- */
-session_start();
-$titre="Connexion";
-include("includes/identifiants.php");
-include("includes/debut.php");
-include("includes/menu.php");
+ * Date: 23/06/2018
+ * Time: 15:52
+*/
+$db = mysqli_connect('localhost', 'root', '', 'trumpbox');
+$email =  mysqli_real_escape_string($db, $_POST['email']);
+$pwd = mysqli_real_escape_string($db, $_POST['pwd']);
+
+if (empty($email)) {
+    array_push($errors, "Adresse email requise");
+}
+if (empty($pwd)) {
+    array_push($errors, "Mot de passe requis");
+}
+
+$pwd = md5($pwd);
+$query = "SELECT * FROM user WHERE email='$email' AND pwd='$pwd'";
+$results = mysqli_query($db, $query);
+if (mysqli_num_rows($results) == 1) {
+    $_SESSION['username'] = $email;
+    $_SESSION['success'] = "Vous êtes maintenant connecté";
 ?>
---
+++ Inscription.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Noron
+ * Date: 23/06/2018
+ * Time: 16:26
+ */
+$db = mysqli_connect('localhost', 'root', '', 'trumpbox');
+$email =  mysqli_real_escape_string($db, $_POST['email']);
+$pwd = mysqli_real_escape_string($db, $_POST['pwd']);
+
+if (empty($email)) { array_push($errors, "Une adresse mail est requis"); }
+if (empty($pwd)) { array_push($errors, "Un mot de passe est requis"); }
+
+$pwd = md5($pwd);
+
+$query = "INSERT INTO user (use_mail, use_password) 
+  			  VALUES('$email', '$pwd')";
+mysqli_query($db, $query);
+$_SESSION['username'] = $email;
+$_SESSION['success'] = "Vous êtes maintenant inscrit";
+}
+?>
+
